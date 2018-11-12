df6254b8ae3fa0e4ea189ae5d5cb907616e27415
splicedbread <jake.vr.84116@gmail.com>
2018-11-11 21:36:23 -0800

    stub change for testing
    thats it

--- Rocket Propelled Game/stub.cpp
+++ Rocket Propelled Game/stub.cpp
@@ -1,123 +1,59 @@
 #include <iostream>
 #include "potion.h"
-#include "string.h"
-#include "dynamic_array.h"
+#include "backpack.h"
 #include <crtdbg.h>
 using std::cin;
 using std::cout;
 using std::endl;
 
-void PotionStub();
+void Stub();
 
 int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	PotionStub();
+	Stub();
 	cin.ignore();
 	return 0;
 }
 
 
-void PotionStub() 
+void Stub() 
 {
-	cout << "///////////////Testing Dynamic Array class Insert///////////////" << endl;
 
+	Potion pt1("Bone Hurting Juice", "Oooo Ohhh, OWCH, this really hurts my bones!", "Mediocre", "10.25.88.109");
 
-	cout << "///////////////Displaying Potions below///////////////" << endl;
+	Potion pt2("Bone Healing Juice", "Oooo Ohhh, OWCH, this really hurts my bones!", "Supreme", "19.40.201.00");
 
-	//Create a dynamic array obj
-
-	DynamicArray<Potion> PotionBelt;
+	Potion pt3("Ains Oal Gown", "I couldn't even think of that!", "Demon Goat", "98.00.00.01");
 	
-	//create 3 potions
+	Potion pt4("Ains Oak Gown", "I couldn't even think of that!", "Demon Goat", "98.00.00.01");
 
-	Potion pt1("Bone Hurting Juice", "Oooo Ohhh, OWCH, this really hurts my bones!", "Super tier", "10.25.88.109");
+	Potion pt5(pt3);
 
-	Potion pt2("Bone Healing Juice", "Oooo Ohhh, OWCH, this really hurts my bones!", "Super tier", "19.40.201.00");
+	pt3.SetPoten("Baby");
 
-	Potion pt3("Ains", "I couldn't even think of that!", "Goat tier", "98.00.00.01");
-	cout << "Displaying a temp thing: " << endl;
+	BackPack bk1(10);
 
-	PotionBelt.Insert(pt1);
-	PotionBelt.Insert(pt2);
-	PotionBelt.Insert(pt3);
-	//Insert two duplicates, these will be deleted to show we only delete one of them
-	PotionBelt.Insert(pt2);
-	PotionBelt.Insert(pt1);
+	bk1.Add(pt1);
+	bk1.Add(pt2);
+	bk1.Add(pt3);
+	bk1.Add(pt1);
+	bk1.Add(pt1);
+	bk1.Add(pt1);
+	bk1.Add(pt2);
+	bk1.Add(pt3);
+	bk1.Add(pt2);
+	bk1.Add(pt3);
 
-	cout << "Number of elements: " << PotionBelt.GetElements() << endl;
+	bk1.Display();
 
-	for (int i = 0; i < PotionBelt.GetElements(); i++)
-	{
-		PotionBelt[i].Display();
-		cout << endl;
-	}
+	bk1.Remove(pt1);
+	bk1.Remove(pt4);
+	bk1.Remove(pt5);
 
-
-	cout << "///////////////Testing Dynamic Array class copy///////////////" << endl;
-
-	DynamicArray<Potion> PotionBelt2;
-
-	PotionBelt2 = PotionBelt;
-
-	cout << "Number of elements PB1: " << PotionBelt.GetElements() << endl;
-	cout << "Number of elements PB2: " << PotionBelt2.GetElements() << endl;
-
-	cout << "///////////////Displaying Belt copy///////////////" << endl;
-
-	for (int i = 0; i < PotionBelt2.GetElements(); i++)
-	{
-		PotionBelt2[i].Display();
-		cout << endl;
-	}
-
-
-	cout << "///////////////Testing Dynamic Array class Delete///////////////" << endl;
-	cout << "///////////////Removing duplicate potions///////////////" << endl;
-
-	PotionBelt2.Delete(pt2);
-	PotionBelt2.Delete(pt1);
-
-	cout << "///////////////Displaying Potions below///////////////" << endl;
-
-	for (int i = 0; i < PotionBelt2.GetElements(); i++)
-	{
-		PotionBelt2[i].Display();
-		cout << endl;
-	}
-
-	cout << "///////////////Comparing Potions together///////////////" << endl;
-
-
-	if (pt1 > pt2)
-	{
-		cout << "pt1 is greater than pt2" << endl;
-	}
-	else
-	{
-		cout << "pt2 is greater than pt1" << endl;
-	}
-
-	if (pt1 < pt2)
-	{
-		cout << "pt1 is less than pt2" << endl;
-	}
-	else
-	{
-		cout << "pt2 is less than pt1" << endl;
-	}
-
-	if (pt1 != pt2)
-	{
-		cout << "pt1 does not equal pt2" << endl;
-	}
-
-	if (pt1 == pt1)
-	{
-		cout << "pt1 does in fact equal pt1" << endl;
-	}
-
+	bk1.UpgradeBP();
+	bk1.Display();
 
 }
 
